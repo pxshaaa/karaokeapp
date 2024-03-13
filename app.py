ea8200f21168e93898c35f74ba6d786bc5ed516f
Pasha Alidadi <pashaalidadi3@gmail.com>
2024-03-13 13:39:16 +0100

    functioning playback non-realtime with microphone

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import os
+import logging
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @app.route('/')
 def index():
+    logger.info('Rendering index.html')
     return render_template('index.html')
 
 @app.route('/menu')
@@ -25,13 +31,6 @@ def rates():
 @app.route('/play_music')
 def play_music():
     return render_template('play.html')
-
-#@app.route('/lyrics/<song>')
-#def display_lyrics(song):
-#    lyrics_file_path = os.path.join(app.root_path, 'lyrics', f'{song}.txt')
-#    with open(lyrics_file_path, 'r') as file:
-#        lyrics = file.read()
-#    return render_template('lyrics.html', lyrics=lyrics)
 
 @app.route('/lyrics/<song>')
 def display_lyrics(song):
@@ -62,11 +61,14 @@ def drinks():
 
 @socketio.on('offer')
 def handle_offer(offer):
+    logger.info('Received offer')
+    print(type(offer))
     emit('offer', offer, broadcast=True)
 
-@socketio.on('answer')
-def handle_answer(answer):
-    emit('answer', answer, broadcast=True)
+#@socketio.on('answer')
+#def handle_answer(answer):
+#    logger.info('Received answer')
+#    emit('answer', answer, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- server.py
+++ server.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
-
-@socketio.on('offer')
-def handle_offer(offer):
-    emit('offer', offer, broadcast=True)
-
-@socketio.on('answer')
-def handle_answer(answer):
-    emit('answer', answer, broadcast=True)
-
-if __name__ == '__main__':
-    socketio.run(app)
+#from flask import Flask, render_template
+#from flask_socketio import SocketIO, emit
+#
+#app = Flask(__name__)
+#app.config['SECRET_KEY'] = 'secret!'
+#socketio = SocketIO(app)
+#
+#@socketio.on('offer')
+#def handle_offer(offer):
+#    emit('offer', offer, broadcast=True)
+#
+#@socketio.on('answer')
+#def handle_answer(answer):
+#    emit('answer', answer, broadcast=True)
+#
+#if __name__ == '__main__':
+#    socketio.run(app)
